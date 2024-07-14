520f592433677bcfa7b3c5d5f5d53333db3addd1
yuliano96 <yulianopacileo@gmail.com>
2024-07-14 13:01:18 +0200

    libft

---
+++ ft_strchr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypacileo <ypacileo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/02 16:32:22 by ypacileo          #+#    #+#             */
+/*   Updated: 2024/06/30 19:04:58 by ypacileo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <stdio.h>
+#include <string.h>*/
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char		ch;
+	char		*ptr;
+
+	ch = (char)c;
+	ptr = (char *)s;
+	while (*ptr != ch && *ptr != '\0')
+		ptr++;
+	if (*ptr == ch)
+		return (ptr);
+	else
+		return (0);
+}
+
+/*int main() {
+    const char *str = "la casa";
+    char *p1 = ft_strchr(str, 'a');
+	char *p2 = strchr(str,'a');
+
+    printf("%s\n",p1);
+	printf("%s\n",p2);
+}*/
